d329495bc8997316bb32b53582c0df0a65699d5f
Gouseazure <117996262+Gouseazure@users.noreply.github.com>
2026-01-05 18:24:32 +0530

    Update main.tf

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -12,6 +12,7 @@ provider "azurerm" {
  
 # Create a Azure Resource Group
 resource "azurerm_resource_group" "aks-rg2" {
+  subscriptions_id = "76ef62c4-a0b6-444e-abb0-f49f4ab60346"
   name     = "aks-rg2-tf"
   location = "Central US"
 
