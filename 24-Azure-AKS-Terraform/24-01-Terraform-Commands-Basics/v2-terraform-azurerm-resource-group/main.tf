fc63246f44648ab7857f91b856c090bd2d0d378c
Gouseazure <117996262+Gouseazure@users.noreply.github.com>
2026-01-06 13:16:50 +0530

    Update main.tf

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -1,11 +1,17 @@
-# Configure Azure Provider
+terraform {
+  required_version = ">= 1.14.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.112"
+    }
+  }
+}
+
 provider "azurerm" {
-  # Version is optional
-  # Terraform recommends to pin to a specific version of provider
-  #version = "=2.35.0"
-  #version = "~>2.35.0"
-  #version = "~> 2.37.0"
   features {}
+  # Optionally set this if not relying on Azure CLI:
+  # subscription_id = "76ef62c4-a0b6-444e-abb0-f49f4ab60346"
 }
 
 # Create a Azure Resource Group
