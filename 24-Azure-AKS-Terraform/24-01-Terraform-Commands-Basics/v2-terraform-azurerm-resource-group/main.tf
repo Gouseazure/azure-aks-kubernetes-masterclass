12839cf29aae3f58d5530f4e149ae6f6ba882cc4
Gouseazure <117996262+Gouseazure@users.noreply.github.com>
2026-01-06 13:05:20 +0530

    Clean up provider configuration comments
    Removed Azure provider configuration comments in main.tf.

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -1,13 +1,3 @@
-# Configure Azure Provider
-provider "azurerm" {
-  # Version is optional
-  # Terraform recommends to pin to a specific version of provider
-  #version = "=2.35.0"
-  #version = "~>2.35.0"
-  #version = "~> 2.37.0"
-  features {}
-}
- 
 # Create a Azure Resource Group
 resource "azurerm_resource_group" "aks-rg2" {
   subscriptions_id = "76ef62c4-a0b6-444e-abb0-f49f4ab60346"
